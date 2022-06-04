f4f253c6be62557c5a6b7a1fd196e8da482eecaa
Kryhowsky <piter71-1997@go2.pl>
2022-06-04 15:40:31 +0200

    Borrower details

--- src/app/public/borrower/borrower-details/borrower-details.component.ts
+++ src/app/public/borrower/borrower-details/borrower-details.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { BorrowerDto } from 'src/api/models';
+import { ClearBorrowerAction, GetBorrowerByIdAction } from '../state/state/borrower.actions';
 
 @Component({
   selector: 'app-borrower-details',
   templateUrl: './borrower-details.component.html',
   styleUrls: ['./borrower-details.component.sass']
 })
-export class BorrowerDetailsComponent implements OnInit {
+export class BorrowerDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  borrowerId: number
+
+  @Select(state => state.borrower.borrower)
+  borrower$: Observable<BorrowerDto>
+
+  formGroup: FormGroup = new FormGroup({})
+
+  constructor(private readonly store: Store, private readonly activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.borrowerId = params["id"]
+
+      if (this.borrowerId) {
+        this.store.dispatch(new GetBorrowerByIdAction(this.borrowerId))
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.store.dispatch(new ClearBorrowerAction());
   }
 
 }
